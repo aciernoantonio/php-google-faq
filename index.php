50f16e6efccd08bd3b372e4f33be7ca4a269aae9
aciernoantonio <aciernoantonio.int@gmail.com>
2022-05-23 17:57:41 +0200

    add complete layout and php

--- index.php
+++ index.php
@@ -72,21 +72,21 @@ $contents = [
             "paragraph_1" => "Il tuo account è associato a un paese (o territorio) nei termini di servizio per poter stabilire due cose:",
 
             "paragraph_2" => "
-            1.    La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi
+            <ol> <li> La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi
             sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
             ",
 
             "paragraph_3" => "
-            a.  Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, 
-            Liechtenstein e Norvegia) o in Svizzera.
+            <ol style='list-style-type: lower-alpha'> <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, 
+            Liechtenstein e Norvegia) o in Svizzera.</li>
             ",
 
             "paragraph_4" => "
-                b.  Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+            <li>    Google LLC, con sede negli Stati Uniti, per il resto del mondo. </li> </ol>
             ",
 
             "paragraph_5" => "
-            2.  La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+            <li> La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali. </li> </ol>
             ",
 
             "paragraph_6" => "
@@ -199,7 +199,7 @@ $contents = [
     </header>
 
 
-    <main class="mt-5">
+    <main class="mt-3">
 
         <div class="container">
 
